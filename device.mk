50125a4d30d3f391f872939d6d327e6af46547a4
Arian <arian.kulmer@web.de>
2024-08-29 21:04:46 +0200

    cupid: sensors: Add v2 suffix to sensors.xiaomi
    Change-Id: Ie6ef0e5fd4653eae298a3455edf14e05508e1cfd

--- device.mk
+++ device.mk
@@ -447,7 +447,7 @@ $(foreach sku, taro diwali cape ukee, \
     ))
 
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
